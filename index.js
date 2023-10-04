24bca8b10b45472811167ff27c9b1e4a75fc3db4
Elad Gildnur <elad.gildnur@gmail.com>
2023-10-04 13:50:40 +0300

    Remove unnecessary files from root dir

--- index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.LavaEthersProvider = void 0;
-// Ethers.js default version is v6
-var lavaEthersProvider_1 = require("./ethersjs/v6/bin/lavaEthersProvider");
-Object.defineProperty(exports, "LavaEthersProvider", { enumerable: true, get: function () { return lavaEthersProvider_1.LavaEthersProvider; } });
