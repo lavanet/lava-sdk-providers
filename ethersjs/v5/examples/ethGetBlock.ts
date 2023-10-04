8706d1e5969af20cf031ee3ef89ad21dd2b9e6d9
Elad Gildnur <elad.gildnur@gmail.com>
2023-10-04 18:56:52 +0300

    Add examples

---
+++ ethersjs/v5/examples/ethGetBlock.ts
@@ -0,0 +1,41 @@
+import {
+  EthersLavaSDKOptions,
+  LavaEthersProvider,
+} from "../src/provider/lavaEthersProvider";
+
+async function createEthersLavaProvider(
+  options: EthersLavaSDKOptions
+): Promise<LavaEthersProvider> {
+  // ## Options A: ##
+  const provider = await LavaEthersProvider.create(options);
+
+  // ## Options B: ##
+  // const provider = new LavaEthersProvider(options);
+  // await provider.init();
+  //
+  //
+  // ## Options C: ##
+  // # In this case, the provider will be initialized automatically on the first request #
+  // const provider = new LavaEthersProvider(options);
+
+  return provider;
+}
+
+async function printLatestBlock() {
+  const ethersProvider = await createEthersLavaProvider({
+    privateKey: process.env.PRIVATE_KEY,
+    chainId: "ETH1",
+    networkId: 1,
+    geolocation: "1",
+    pairingListConfig: process.env.PAIRING_LIST_CONFIG_PATH,
+    lavaChainId: "lava",
+    logLevel: "info",
+    allowInsecureTransport: true,
+  });
+
+  const latestBlock = await ethersProvider.getBlock("latest");
+
+  console.log(latestBlock);
+}
+
+(async (): Promise<void> => await printLatestBlock())();
