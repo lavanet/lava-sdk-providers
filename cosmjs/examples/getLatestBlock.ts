bb614f01169b67331daab8db1fad796a1fe2e200
Elad Gildnur <6321801+shleikes@users.noreply.github.com>
2023-10-08 20:04:21 +0300

    PRT-905 SDK: CosmJS integration (#19)
    * Init
    
    * Add devDependencies and scripts to package.json
    
    * Add lavaWeb3jsProvider
    
    * Add lavaWeb3jsProvider example
    
    * Remove unnecessary files from root dir
    
    * Rename lavaWeb3jsProvider.ts -> lavaWeb3Provider.ts
    
    * Add README to Web3js and update the root README file
    
    * Update the example
    
    * Update v6
    
    * Update v5
    
    * Update README
    
    * Fix after using lavasdk
    
    * Add examples
    
    * Init files
    
    * Add lavaCosmjsRpcClient
    
    * Update README
    
    ---------
    
    Co-authored-by: Ran Mishael <106548467+ranlavanet@users.noreply.github.com>

---
+++ cosmjs/examples/getLatestBlock.ts
@@ -0,0 +1,63 @@
+import { LavaSDKOptions } from "@lavanet/lava-sdk";
+import {
+  Tendermint34Client,
+  Tendermint37Client,
+  TendermintClient,
+  Comet,
+} from "@cosmjs/tendermint-rpc";
+
+import {} from "@cosmjs/tendermint-rpc";
+
+import { LavaCosmJsRPCClient } from "../src/lavaCosmjsRpcClient";
+
+async function createTendermintClientInstance(
+  rpcClientName: string,
+  options: LavaSDKOptions
+): Promise<TendermintClient> {
+  // ## Options A: ##
+  const client = await LavaCosmJsRPCClient.create(options);
+
+  // ## Options B: ##
+  // const provider = new LavaCosmJsRPCClient(options);
+  // await provider.init();
+  //
+  //
+  // ## Options C: ##
+  // # In this case, the provider will be initialized automatically on the first request #
+  // const provider = new LavaCosmJsRPCClient(options);
+
+  switch (rpcClientName) {
+    case "tendermint34":
+      return Tendermint34Client.create(client);
+    case "tendermint37":
+      return Tendermint37Client.create(client);
+    default:
+      throw new Error(`Unknown RPC client: ${rpcClientName}`);
+  }
+}
+
+async function printLatestBlock(rpcClientName: string) {
+  const tendermingClient = await createTendermintClientInstance(rpcClientName, {
+    privateKey: process.env.PRIVATE_KEY,
+    chainIds: "LAV1",
+    geolocation: "1",
+    pairingListConfig: process.env.PAIRING_LIST_CONFIG_PATH,
+    lavaChainId: "lava",
+    logLevel: "info",
+    allowInsecureTransport: true,
+  });
+
+  const latestBlock = await tendermingClient.abciInfo();
+  return latestBlock;
+}
+
+(async (): Promise<void> => {
+  const rpcClientNames = ["tendermint34", "tendermint37"];
+  rpcClientNames.forEach(async (rpcClientName, _) => {
+    const latestBlock = await printLatestBlock(rpcClientName);
+
+    console.log(`##### Using rpcClient: ${rpcClientName} #####`);
+    console.log(latestBlock);
+    console.log("\n\n");
+  });
+})();
