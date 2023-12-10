02e46ef44afb8d75ec8e0793d6caf644789f4a66
Ran Mishael <106548467+ranlavanet@users.noreply.github.com>
2023-12-10 13:19:50 +0100

    fixed all publish related configurations to publish the dist instead of the src files. (#24)

---
+++ scripts/deploy_all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# cd ./cosmjs; echo -ne '\n' | yarn publish; cd ..
+cd ./ethersjs/v5; echo -ne '\n' | yarn publish; cd ../..
+cd ./ethersjs/v6; echo -ne '\n' | yarn publish; cd ../..
+cd ./viem; echo -ne '\n' | yarn publish; cd ..
+cd ./web3js; echo -ne '\n' | yarn publish; cd ..
